c27914922a51a61a8ce610b287b762a4ec8086cc
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-04-13 15:52:08 -0500

    undefined method error in app/views/articles/new.html.erb ---form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'a3210ce9714ea3d7becc72b0f25ca0a882f0549a9c8cd0c12b3bbec380d399ea254bd5d0f966633e1ccd671eacd96c2981faa3574a4edfedac329fdffbca6047'
